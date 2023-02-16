8b4d0d358222c1426dd3e4b0fe472156f06b8148
Monica Pena <113930652+monica-ap@users.noreply.github.com>
2023-02-16 18:40:33 +0000

    learned how to read and write in python.....

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-# remember to fork this and push to github
+# # remember to fork this and push to github
 
+
+# f =open('emploees.txt', 'a')
+# f.read("and john")
+# #read gives us everything
+# #readline gives us the firstline
+# print(f.read())
+# f.close()
+
+# with open("test.txt",'w') as f:
+#   f.write("rgoeirgjijgirjoisdfoseijgsjdgdgoirjgirjifjsfpoggji")
+#   print(f)
+
+with open('period7.txt','w') as f:
+  f.write("this mess is over")
